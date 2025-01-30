83835b444cd6e3163d91c503e637789a4a4d4e8f
Philippcmd <139921614+Philippcmd@users.noreply.github.com>
2025-01-30 13:26:23 +0100

    Initial Commit

--- src/main/java/dev/philippcmd/commandControl/CommandListener.java
+++ src/main/java/dev/philippcmd/commandControl/CommandListener.java
@@ -1,4 +1,23 @@
 package dev.philippcmd.commandControl;
 
-public class CommandListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class CommandListener implements Listener {
+
+    private final CommandControl plugin;
+
+    public CommandListener(CommandControl plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        String command = event.getMessage().split(" ")[0].substring(1).toLowerCase();
+        if (plugin.isCommandDeactivated(command)) {
+            event.getPlayer().sendMessage("This command is currently deactivated.");
+            event.setCancelled(true);
+        }
+    }
 }
