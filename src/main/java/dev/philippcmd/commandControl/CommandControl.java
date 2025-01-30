9c6ee86dd225e48c6d5f33c9055b8299037f3363
Philippcmd <139921614+Philippcmd@users.noreply.github.com>
2025-01-30 13:25:02 +0100

    Initial commit

---
+++ src/main/java/dev/philippcmd/commandControl/CommandControl.java
@@ -0,0 +1,17 @@
+package dev.philippcmd.commandControl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CommandControl extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/dev/philippcmd/commandControl/CommandListener.java
@@ -0,0 +1,4 @@
+package dev.philippcmd.commandControl;
+
+public class CommandListener {
+}
